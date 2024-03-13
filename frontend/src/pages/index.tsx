0235dc9a06e2fac8a1a39d48a2eddcb1c0323075
oginom <oginomaa@gmail.com>
2024-03-13 22:50:55 +0900

    create domain types

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,20 +1,40 @@
 import { NextPage } from 'next';
 import React, { useRef, useState } from 'react';
-import { Canvas, useFrame } from '@react-three/fiber';
-import { Mesh, Vector3 } from 'three';
+import { Canvas, useFrame, MeshProps, GroupProps } from '@react-three/fiber';
+import { Mesh, OrthographicCamera as OrthographicCamera_three, Vector3 } from 'three';
 import { Text, OrthographicCamera } from '@react-three/drei';
+
+// domain types
+
+type Suit = "Spade" | "Heart" | "Diamond" | "Club";
+type Card = {
+  number: number;
+  suit: Suit;
+};
+
+type Field = {
+  card0: Card;
+  card1: Card;
+  card2: Card;
+  card3: Card;
+  card4: Card;
+}
+
+type Game = {
+  field: Field;
+}
+
+// component types
 
 type BoxProps = {
   position: [x: number, y: number, z: number];
 };
 
 type TrumpProps = {
-  position: [x: number, y: number, z: number];
-  number: number;
-  suit: string;
+  card: Card;
 };
 
-const Trump: React.FC<TrumpProps> = (props) => {
+const Trump: React.FC<TrumpProps & MeshProps> = (props) => {
   const mesh = useRef<Mesh>(null!);
   const [hovered, setHover] = useState(false);
   const [active, setActive] = useState(false);
@@ -34,17 +54,27 @@ const Trump: React.FC<TrumpProps> = (props) => {
         fontSize={0.1}
         color={'#000'}
       >
-        {props.suit}
+        {props.card.suit}
       </Text>
       <Text
         position={[0, 0, 0.05]}
         fontSize={0.2}
         color={'#000'}
       >
-        {props.number}
+        {props.card.number}
       </Text>
     </mesh>
   );
+}
+
+const Field: React.FC<{ field: Field } & GroupProps> = (props) => {
+  return <group {...props}>
+    <Trump position={[-2.4, 0, 0]} card={props.field.card0} />
+    <Trump position={[-1.2, 0, 0]} card={props.field.card1} />
+    <Trump position={[0, 0, 0]} card={props.field.card2} />
+    <Trump position={[1.2, 0, 0]} card={props.field.card3} />
+    <Trump position={[2.4, 0, 0]} card={props.field.card4} />
+  </group>
 }
 
 const Box: React.FC<BoxProps> = (props) => {
@@ -71,7 +101,10 @@ const Box: React.FC<BoxProps> = (props) => {
 const Rig = ({ v = new Vector3() }) => {
   return useFrame((state) => {
     //state.camera.position.lerp(v.set(state.mouse.x / 2, state.mouse.y / 2, 10), 0.05)
-    //state.camera.position.set(0, 0, 1);
+    (state.camera as OrthographicCamera_three).top = 2;
+    (state.camera as OrthographicCamera_three).bottom = -2;
+    (state.camera as OrthographicCamera_three).left = -3;
+    (state.camera as OrthographicCamera_three).right = 3;
   })
 };
 
@@ -79,19 +112,26 @@ const Home: NextPage = () => {
   const L = -3;
   const R = 3;
   const T = 2;
-  const B = -2+0;
+  const B = -2;
 
-  return <div style={{ width: '100vw', height: '100vh' }}>
-    <Canvas>
-      <OrthographicCamera makeDefault position={[0, 0, 1]} left={L} right={R} top={T} bottom={B} near={0.1} far={1000} />
-      <ambientLight />
-      <pointLight position={[10, 10, 10]} />
-      <Rig />
-      <Trump position={[-2.4, 0, 0]} number={5} suit={"Spade"} />
-      <Trump position={[-1.2, 0, 0]} number={5} suit={"Spade"} />
-      <Trump position={[0, 0, 0]} number={5} suit={"Spade"} />
-      <Trump position={[1.2, 0, 0]} number={5} suit={"Spade"} />
-      <Trump position={[2.4, 0, 0]} number={5} suit={"Spade"} />
+  const game: Game = {
+    field: {
+      card0: {number: 5, suit: "Spade"},
+      card1: {number: 5, suit: "Spade"},
+      card2: {number: 5, suit: "Spade"},
+      card3: {number: 5, suit: "Spade"},
+      card4: {number: 5, suit: "Spade"},
+    }
+  }
+
+  return <div style={{ width: '99vw', height: '66vw' }}>
+    <Canvas flat linear>
+      <OrthographicCamera makeDefault position={[0, 0, 0]} left={L} right={R} top={T} bottom={B} near={0.1} far={1000} >
+        <ambientLight />
+        <pointLight position={[10, 10, 10]} />
+        <Rig />
+        <Field field={game.field} position={[0, 0, -2]} scale={0.5}/>
+      </OrthographicCamera>
     </Canvas>
   </div>
 };
